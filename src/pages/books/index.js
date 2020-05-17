f7b5bbec374ea8b5ba1e17df4bb796144988322d
Guilherme Girardi <girardi.gui@icloud.com>
2020-05-17 15:49:44 +0100

    create api

--- src/pages/books/index.js
+++ src/pages/books/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { GridContainer } from '../styles/styles';
 
 export default function Books() {
+  const [books, setBooks] = useState('');
+  const [result, setResult] = useState([]);
+
+  function handleChange(e) {
+    const book = e.target.value;
+    setBooks(book);
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios.get('https://www.googleapis.com/books/v1/volumes?q=' + books + '&maxResults=40')
+    .then(res => {
+      setResult(res.data.items);
+    })
+  };
+
+
   return (
     <>
       <GridContainer>
-        <div className="books">
-          <h1>Books</h1>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              onChange={handleChange}
+              placeholder="Search a book on Google"
+              autoComplete="off"
+            />
+          </div>
+          <button type="submit">Search</button>
+        </form>
+        {result.map(book =>
+          <a key={book.id} href={book.volumeInfo.previewLink}>
+          <img src={book.volumeInfo.imageLinks.thumbnail} alt="Book" />
+          </a>
+        )}
       </GridContainer>
     </>
   )
