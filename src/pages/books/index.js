ce05c5cac5e68588ddb309368efa01b6cd2bdf9e
Guilherme Girardi <girardi.gui@icloud.com>
2020-05-17 18:25:32 +0100

    review books/index.js

--- src/pages/books/index.js
+++ src/pages/books/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { GridContainer } from '../styles/styles';
 
@@ -6,7 +6,10 @@ export default function Books() {
   const [books, setBooks] = useState('');
   const [result, setResult] = useState([]);
 
-  function handleChange(e) {
+  const [loadBooks, setLoadBooks] = useState([]);
+  const [error, setError] = useState(null);
+
+  function handleInput(e) {
     const book = e.target.value;
     setBooks(book);
   };
@@ -14,11 +17,19 @@ export default function Books() {
   function handleSubmit(e) {
     e.preventDefault();
     axios.get('https://www.googleapis.com/books/v1/volumes?q=' + books + '&maxResults=40')
-    .then(res => {
-      setResult(res.data.items);
-    })
+    .then(res => setResult(res.data.items))
+    .catch(err => setError(err));
   };
 
+
+  function fetchBooks() {
+    axios.get('https://www.googleapis.com/books/v1/volumes?q=harry+potter')
+    .then(response => setLoadBooks(response.data.items))
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, [])
 
   return (
     <>
@@ -27,18 +38,28 @@ export default function Books() {
           <div>
             <input
               type="text"
-              onChange={handleChange}
+              onChange={handleInput}
               placeholder="Search a book on Google"
               autoComplete="off"
             />
           </div>
           <button type="submit">Search</button>
         </form>
+
+
+        {loadBooks.map(book =>
+          <a key={book.id} href={book.volumeInfo.previewLink}>
+          <img src={book.volumeInfo.imageLinks.thumbnail} alt="Book" />
+          </a>
+        )}
+
         {result.map(book =>
           <a key={book.id} href={book.volumeInfo.previewLink}>
           <img src={book.volumeInfo.imageLinks.thumbnail} alt="Book" />
           </a>
         )}
+
+
       </GridContainer>
     </>
   )
